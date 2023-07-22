35c8af4deaf5b908bed4aa55dbbc68d64f9177e2
nishu123a <85247471+nishu123a@users.noreply.github.com>
2023-07-22 08:33:24 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Routes from "./Routes";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(<Routes />, document.getElementById("root"));
+/*import { createRoot } from "react-dom";
+
+import App from "./App";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);*/
+
